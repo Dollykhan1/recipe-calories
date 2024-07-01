d70709fc0ab292588f5279d3cfb8b8daee0d0c46
Dollykhan1 <dollykb35@gmail.com>
2024-07-01 08:46:32 -0400

    first commit

---
+++ src/Component/Recipe_last/Recipe_last.jsx
@@ -0,0 +1,12 @@
+const Recipe_last = () => {
+    return (
+        <div className="text-center">
+            <h1 className="text-4xl font-semibold">Our Recipes</h1>
+            <p className="text-base text-[#150B2B99] mt-5">
+            Mouthwatering  recipes Americans are missing out on. There are some traditional American foods,such as <br /> hearty stews and comforting potato dishes,that are less likely to be found stateside  
+            </p>
+        </div>
+    );
+};
+
+export default Recipe_last;
